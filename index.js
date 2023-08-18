a4d89b8068a4ced98607e5e702f2ba996a052813
ana03ap <anxrdilx03@gmail.com>
2023-08-17 20:26:35 -0500

    agregando botones

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 // para la api de rick morty
-const url = "https://rickandmortyapi.com/api/character";
+// const url = "https://rickandmortyapi.com/api/character";
 const main = document.getElementById("root");
+const search = document.getElementById('container-buscador')
+const arrowPpl= document.getElementById('arrow-down')
+const element1 = document.getElementsByClassName('element')[0]
+
+
+
+
 
 // para cambiar el modo de la pagina 
 const root = document.documentElement;
 const btn = document.getElementById("mode")
 let sw= false
+// crear variables de los botones para las cards
+const btnBefore= document.getElementById("beforeBtn")
+const btnAfter= document.getElementById("afterBtn")
+
+// para numero de pagi
+let numPagina=1
+const buscar = getElementById('buscar')
+const input =getElementById('input-a-buscar')
+buscar.addEventListener('click',()=>{
+    input.value
+    
+})
+
+
+
 
 btn.addEventListener("click",()=>{
     if(sw){
@@ -24,46 +46,64 @@ btn.addEventListener("click",()=>{
 //     root.style.setProperty('--color-text', '#fff')
 // })
 
-fetch(url)
-.then((response) => {
-    return response.json();
-})
-.then((data) => {
-    console.log(data);
-    const Info = data.results;
-    console.log(Info);
 
-    Info.map((element) => {
-    const nameToShow =
-        element.name.length > 12
-        ? element.name.slice(0, 11) + ".."
-          : element.name; // si es mayor de 12 escribir 10 y concatenarle ".."
-      // if (element.name.length > 12){
-      //     nameToShow= element.name.slice(0, 11) + ".."
-      // }else{
-      //     nameToShow= element.name
-      // }
-
-    main.innerHTML += `
-        <div class="cards">
-            <div class="nombre_img">
-                <h1 class = "name">${nameToShow}</h1>
+function getinfo(numberPage){
+    const url = `https://rickandmortyapi.com/api/character?page=${numberPage}`;// para que 
+    fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+        const Info = data.results;
+        console.log(Info);
+        main.innerHTML =""
+        Info.map((element) => {
+        const nameToShow =
+            element.name.length > 12
+            ? element.name.slice(0, 11) + ".."
+            : element.name; // si es mayor de 12 escribir 10 y concatenarle ".."
+        
+        main.innerHTML += `
+            <div class="cards">
+                <div class="nombre_img">
+                    <h1 class = "name">${nameToShow}</h1>
+                    
+                    <!-- <br> -->
+                    <img class = "image"src="${element.image}" alt=${element.name}/>
+                </div>
+                <div class="cards_info">
+                <p class="status">Status: ${element.status}</p>
+                    <p class="species">specie: ${element.species}</p>
+                    <p class="gender">gender: ${element.gender}</p>
+                    
+                </div>
                 
-                <!-- <br> -->
-                <img class = "image"src="${element.image}" alt=${element.name}/>
             </div>
-            
-            <div class="cards_info">
-            <p class="status">Status: ${element.status}</p>
-                <p class="species">specie: ${element.species}</p>
-                <p class="gender">gender: ${element.gender}</p>
-                
-            </div>
-            
-        </div>
-        `;
+            `;
+        });
+    })
+    .catch((err) => {
+        console.log(err);
     });
+
+}
+
+getinfo(numPagina);
+
+btnBefore.addEventListener('click',()=>{
+    numPagina--;
+    getinfo(numPagina);
+    search.scrollIntoView({behavior: 'smooth'})
 })
-.catch((err) => {
-    console.log(err);
-});
+
+btnAfter.addEventListener('click',()=>{
+    numPagina++;
+    getinfo(numPagina);
+    search.scrollIntoView({behavior: 'smooth'})
+})
+
+
+arrowPpl.addEventListener('click',()=>{
+    element1.scrollIntoView({behavior: 'smooth'})
+})
