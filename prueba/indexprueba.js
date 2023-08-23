b66049db58986aef75b75187c8cc494cdfc95bc2
ana03ap <anxrdilx03@gmail.com>
2023-08-22 19:48:25 -0500

    a

--- prueba/indexprueba.js
+++ prueba/indexprueba.js
@@ -4,6 +4,7 @@ const main = document.getElementById("root");
 const search = document.getElementById('container-buscador')
 const arrowPpl= document.getElementById('arrow-down')
 const element1 = document.getElementsByClassName('element')[0]
+const obtenerNombres = [];// para guardar todos los nombres 
 
 
 // para cambiar el modo de la pagina 
@@ -25,11 +26,22 @@ getInfo(numPagina,busqueda)
 
 // AQUI VOY A PONER EL BUSCADOR PERO DE LA FORMA EN LA QUE LA HICE KEYNER
 buscar.addEventListener('click',()=>{// cuando se oprime el boton de buscar qué pasa?!
-    
     const busqueda  = input.value
     console.log(busqueda)
+    ompararInputArr(busqueda)
+    // cuando obtiene lo que quiere, lo compara con el array de obtenerNombres que tiene toda la info
+
     getInfo(numPagina,busqueda)
 })
+
+function compararInputArr(busqueda){
+    // se ejecuta esto que devuelve el array con toda la info
+    obtenerNombre();// corre la primera vez para tener la info guardada
+    const arrypag=[]
+
+    
+
+}
 
 
 
@@ -45,6 +57,32 @@ btn.addEventListener("click",()=>{
         sw= true
     }
 })
+
+
+// function que obtiene la info de todos los nombres
+
+function obtenerNombre(){
+    // recibe array obtener nonbres que esta global
+    for (let i = 1; i <= 42; i++) {// 42 porque tiene 42 paginas
+        // entrar a la api 42 veces y meter todos los nombres en un array de array, para a la vez guardaar la pagina donde esta 
+        // siendo la pagina la posicion del array principal 
+        const url = `https://rickandmortyapi.com/api/character?page=${i}`;// para que 
+        fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            // console.log(data);
+            const Info = data.results;
+            // agregar a obtenerNombres
+            obtenerNombres.push(Info);// el espacio del array es el numero de la pagina, recordar que tiene que ser posición +1
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+        // more statements
+    }
+}
 
 
 
